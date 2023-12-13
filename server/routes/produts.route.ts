2de766678b3a7d197c512eb69b4cb0860ec88fbe
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-13 08:12:25 -0500

    [backend] products.route.ts modified, routes getAll and post created

--- server/routes/produts.route.ts
+++ server/routes/produts.route.ts
@@ -1,13 +1,14 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { Products } from "../controllers/products.controller";
 
 
 const router = Router();
 
-router.get('/')
+router.get('/', (req : Request, res: Response) => new Products().getAll(res) )
+router.post('/', (req : Request, res: Response) => new Products().post(req, res) )
 router.get('/:id')
 router.get('/:category')
 router.put('/:id')
-router.post('/')
 router.delete('/:id')
 
 export default router
