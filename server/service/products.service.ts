c37903749c7263fb07aeb9c5e3274c0eac2576ab
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-13 08:18:20 -0500

    [backend] products.service.ts modified, methods findAll and create, configured

---
+++ server/service/products.service.ts
@@ -0,0 +1,30 @@
+// import { Product } from "../models/product.entity";
+// import { Repository } from '../interface/repository';
+import { MysqlRepository } from "../interface/products.repository";
+import { Request, Response } from 'express';
+
+export class ProductService {
+  private repository;
+
+  constructor() {
+    this.repository = new MysqlRepository();
+  }
+
+  async findAll(res: Response) {
+    try {
+      const response = await this.repository.getAll(res);
+      return response;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+
+  async create(req : Request, res : Response) {
+    try {
+      const response = await this.repository.create(req, res);
+      return response;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+}
